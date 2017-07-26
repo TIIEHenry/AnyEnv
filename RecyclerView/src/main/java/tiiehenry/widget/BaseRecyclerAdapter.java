ed3199e5239e6e94464c93ce3432a7749b7bb0f6
TIIEHenry <1806968131@qq.com>
2017-07-27 01:40:18 +0800

    新增最强RecyclerAdapter

---
+++ RecyclerView/src/main/java/tiiehenry/widget/BaseRecyclerAdapter.java
@@ -0,0 +1,64 @@
+package tiiehenry.widget;
+
+import android.view.*;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import java.util.List;
+
+public abstract class BaseRecyclerAdapter<T> extends RecyclerView.Adapter<ViewHolder> {
+
+
+  protected List<T> dataList;
+
+  public BaseRecyclerAdapter(List<T> dataList) {
+	super();
+	this.dataList = dataList;
+  }
+
+  @Override
+  public abstract int getItemViewType(int position)
+
+  @Override
+  public abstract ViewHolder onCreateViewHolder(ViewGroup parent, int pos)
+
+  @Override
+  public void onBindViewHolder(ViewHolder<T> holder, int pos) {
+	T data=dataList.get(pos);
+	holder.setData(data);
+	bindData(holder,data, getItemViewType(pos), pos);
+  }
+
+  public List<T> getDataList() {
+	return dataList;
+  }
+
+  @Override
+  public int getItemCount() {
+	return dataList.size();
+  }
+
+  public void add(T data) {
+	dataList.add(data);
+	notifyDataSetChanged();
+  }
+  public void add(int pos, T data) {
+	dataList.add(pos, data);
+	notifyDataSetChanged();
+  }
+  public void remove(T data) {
+	dataList.remove(data);
+	notifyDataSetChanged();
+  }
+  public void remove(int pos) {
+	dataList.remove(pos);
+	notifyDataSetChanged();
+  }
+  public void clear() {
+	dataList.clear();
+	notifyDataSetChanged();
+  }
+
+  protected abstract void bindData(ViewHolder<T> holder,T data, int type, int pos);
+
+}
---
+++ RecyclerView/src/main/java/tiiehenry/widget/ViewHolder.java
@@ -0,0 +1,60 @@
+package tiiehenry.widget;
+
+import android.widget.*;
+import android.support.v7.widget.RecyclerView;
+import android.util.SparseArray;
+import android.view.View;
+import java.util.List;
+
+public abstract class ViewHolder<T> extends RecyclerView.ViewHolder implements View.OnLongClickListener, View.OnClickListener {
+
+  // SparseArray 比 HashMap 更省内存，在某些条件下性能更好，只能存储 key 为 int 类型的数据，
+  // 用来存放 View 以减少 findViewById 的次数
+  private SparseArray<View> viewSparseArray;
+
+  private View rootView;
+  private T data;
+  public ViewHolder(View itemView) {
+	super(itemView);
+	itemView.setOnClickListener(this);
+	itemView.setOnLongClickListener(this);
+	rootView = itemView;
+	viewSparseArray = new SparseArray<>();
+  }
+  
+  public void setData(T data) {
+	this.data = data;
+  }
+
+  public T getData() {
+	return data;
+  }
+
+  public View getView(int viewId) {
+	// 先从缓存中找，找打的话则直接返回
+	// 如果找不到则 findViewById ，再把结果存入缓存中
+	View view = viewSparseArray.get(viewId);
+	if (view == null) {
+	  view = itemView.findViewById(viewId);
+	  viewSparseArray.put(viewId, view);
+	}
+	return view;
+  }
+  public View getRootView() {
+	return rootView;
+  }
+
+  @Override
+  public void onClick(View rootView) {
+	onItemClick(rootView, data, getAdapterPosition());
+  }
+  @Override
+  public boolean onLongClick(View rootView) {
+	return onItemLongClick(rootView,data, getAdapterPosition());
+  }
+
+public abstract void onItemClick(View rootView, T data, int pos);
+ public abstract boolean onItemLongClick(View rootView, T data, int pos);
+
+
+}
