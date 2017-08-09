3e06034528fa2baa3106c9ad38eb3cdb843146ad
TIIEHenry <1806968131@qq.com>
2017-08-09 20:38:10 +0800

    优化Static Method 的Hook

--- AndHookHelper/src/main/java/tiiehenry/hook/HookHelper.java
+++ AndHookHelper/src/main/java/tiiehenry/hook/HookHelper.java
@@ -7,8 +7,9 @@ import tiiehenry.hook.Hook;
 import apk.andhook.AndHook;
 
 public class HookHelper extends AndHook.HookHelper {
-  
+
   public void applyHooks(Class<?> holdClass) throws Exception,SecurityException, NoSuchMethodException {
+	AndHook.ensureClassInitialized(holdClass);
 	for (Method hookMethod : holdClass.getDeclaredMethods()) {
 	  Hook hook = hookMethod.getAnnotation(Hook.class);
 	  if (hook != null) {
