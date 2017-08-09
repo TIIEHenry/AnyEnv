aedd137ad8cd8bad40eab980ba69044b9491de29
TIIEHenry <1806968131@qq.com>
2017-08-09 20:34:15 +0800

    加入throw Exception

--- AndHookHelper/src/main/java/tiiehenry/hook/HookHelper.java
+++ AndHookHelper/src/main/java/tiiehenry/hook/HookHelper.java
@@ -7,16 +7,14 @@ import tiiehenry.hook.Hook;
 import apk.andhook.AndHook;
 
 public class HookHelper extends AndHook.HookHelper {
-  public void applyHooks(Class<?> holdClass) {
+  
+  public void applyHooks(Class<?> holdClass) throws Exception,SecurityException, NoSuchMethodException {
 	for (Method hookMethod : holdClass.getDeclaredMethods()) {
 	  Hook hook = hookMethod.getAnnotation(Hook.class);
 	  if (hook != null) {
 		Method origin=null;
 		AndHook.ensureClassInitialized(hook.clazz());
-		try {
-		  origin = hook.clazz().getDeclaredMethod(hook.method(), (Class[])hook.param());
-		} catch (SecurityException|NoSuchMethodException e) {
-		}
+		origin = hook.clazz().getDeclaredMethod(hook.method(), (Class[])hook.param());
 		if (origin == null)
 		  return;
 		hook(origin, hookMethod);
